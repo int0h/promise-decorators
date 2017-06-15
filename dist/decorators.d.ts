293afe3fa66a230d497f17a0d7b19c52f117cd45
int0h <int0h@yandex.ru>
2017-06-16 01:47:58 +0300

    refactoring

--- dist/decorators.d.ts
+++
@@ -1,9 +0,0 @@
-import { AsyncFunction, WatchHandler } from './index';
-export interface AsyncFunctionDecorator<T> {
-    (target: any, key: string, descriptor: any): {
-        value: AsyncFunction<T>;
-    };
-}
-export declare function asyncTransform<T>(transform: Function): AsyncFunctionDecorator<T>;
-export declare function takeLast<T>(): AsyncFunctionDecorator<T>;
-export declare function watchPromise<T>(handler: WatchHandler): AsyncFunctionDecorator<T>;
---
+++ src/wiki.ts
@@ -0,0 +1,42 @@
+import {throttle, takeLast, watchPromise, asyncTransform} from 'promise-decorators/decorators';
+
+console.log('started');
+
+const text = document.getElementById('text') as HTMLInputElement;
+const results = document.getElementById('results');
+const loader = document.getElementById('loader');
+
+class Api {
+    host: string;
+
+    constructor (host: string) {
+        this.host = host;
+    }
+
+    @watchPromise(toggleLoader)
+    @takeLast()
+    @throttle(1500)
+    search(text: string) {
+        return fetch(this.host + `?action=opensearch&format=json&origin=*&search=${encodeURIComponent(text)}&limit=10`);
+    }
+}
+
+const api = new Api('https://en.wikipedia.org//w/api.php');
+
+function toggleLoader(status: string) {
+    if (status === 'pending') {
+        loader.style.display = 'block';
+        results.style.display = 'none';
+    } else {
+        loader.style.display = 'none';
+        results.style.display = 'block';
+    };
+}
+
+text.oninput = () => text.value.length > 2 && api.search(text.value)
+    .then(res => res.json())
+    .then(res => {
+        results.innerHTML = res[1]
+            .map((item: string) => `<li>${item}</li>`)
+            .join('');
+    });
