64eb39c7d3548887580a416343a7ad523dac1050
int0h <int0h@yandex.ru>
2017-07-12 01:20:12 +0300

    remove dist

--- dist/modules/asyncTransform.d.ts
+++
@@ -1,9 +0,0 @@
-import { AsyncFunction } from '../index';
-/**
- * Transforms a given promise-function with a transform function
- *
- * @param fn - source promise-function
- * @param transform - function which take a function, do some stuff with it and returns a new one
- * e.g. _.throttle (from lodash)
- */
-export declare function asyncTransform<T>(fn: AsyncFunction<T>, transform: Function): AsyncFunction<T>;
--- dist/tests/helpers.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function fetch(data: any, pause?: number): Promise<any>;
